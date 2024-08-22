4a6ebdd562af650823b4c710edf5ebbbfaca28f0
Jr Nolasco <31513935+wmnjunior@users.noreply.github.com>
2024-08-22 16:24:04 -0300

    Ver 1.0
    Primeira ver

---
+++ script.js
@@ -0,0 +1,21 @@
+﻿function redeemCoupon() {
+    const couponInput = document.getElementById("couponInput").value;
+    const validCoupon = "GELSTUDIOTOTI";
+
+    if (couponInput === validCoupon) {
+        // Exibe a imagem do vale com a data e hora
+        const resultContainer = document.getElementById("resultContainer");
+        resultContainer.classList.remove("hidden");
+
+        // Obtém a data e hora atuais
+        const now = new Date();
+        const formattedDate = now.toLocaleDateString();
+        const formattedTime = now.toLocaleTimeString();
+
+        // Insere a data e hora na imagem do vale
+        const timestamp = document.getElementById("timestamp");
+        timestamp.textContent = `${formattedDate} ${formattedTime}`;
+    } else {
+        alert("Cupom inválido! Tente novamente.");
+    }
+}
