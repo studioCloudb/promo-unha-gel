3bb3f908931b0119dd37c32250adc458b8317efc
Jr Nolasco <31513935+wmnjunior@users.noreply.github.com>
2024-08-22 18:51:09 -0300

    2.4

--- script.js
+++ script.js
@@ -5,6 +5,7 @@
     if (coupon === validCoupon) {
         const voucherImage = document.getElementById("voucher-image");
         const voucherDate = document.getElementById("voucher-date");
+        const whatsappLink = document.getElementById("whatsapp-link");
 
         // Gerar data e hora atual formatada
         const now = new Date();
@@ -13,6 +14,10 @@
         // Exibir a imagem e a data/hora no canto inferior direito
         voucherDate.textContent = dateString;
         voucherImage.style.display = "block";
+
+        // Atualizar o link do WhatsApp com o novo texto e o link da imagem
+        const whatsappMessage = `Estou entrando em contato para agendar o serviço usando o cupom promocional: *GELSTUDIOTOTI*. Veja o vale: https://encr.pw/VALESTUDIOTOTI`;
+        whatsappLink.href = `https://wa.me/5527992021861?text=${encodeURIComponent(whatsappMessage)}`;
     } else {
         alert("Cupom inválido. Tente novamente.");
     }
