ecd94dd64dd42ec0a77b9547fc0458f5539e2e7a
Jr Nolasco <31513935+studioCloudb@users.noreply.github.com>
2024-08-22 19:51:50 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -5,29 +5,20 @@
     if (coupon === validCoupon) {
         const voucherImage = document.getElementById("voucher-image");
         const voucherDate = document.getElementById("voucher-date");
+        const whatsappLink = document.getElementById("whatsapp-link");
 
         // Gerar data e hora atual formatada
         const now = new Date();
         const dateString = now.toLocaleDateString('pt-BR') + ' ' + now.toLocaleTimeString('pt-BR');
 
         // Exibir a imagem e a data/hora no canto inferior direito
-        voucherDate.textContent = dateString;
+        voucherDate.textContent = `Data e Hora: ${dateString}`;
         voucherImage.style.display = "block";
+
+        // Atualizar o link do WhatsApp com o novo texto e o link da imagem
+        const whatsappMessage = `Estou entrando em contato para agendar o serviço usando o cupom promocional: *GELSTUDIOTOTI*. Gerado no dia *${dateString}*. Veja o vale: https://studiocloudb.github.io/promo-unha-gel/vale.png`;
+        whatsappLink.href = `https://wa.me/5527992021861?text=${encodeURIComponent(whatsappMessage)}`;
     } else {
         alert("Cupom inválido. Tente novamente.");
     }
 });
-
-function copyToClipboardAndOpenWhatsApp() {
-    // Texto a ser copiado
-    const message = `Estou entrando em contato para agendar o serviço usando o cupom promocional: *GELSTUDIOTOTI*. Veja o vale: https://wmnjunior.github.io/promo-unha-gel/vale.png`;
-
-    // Copiar o texto para a área de transferência
-    navigator.clipboard.writeText(message).then(function () {
-        // Abrir o WhatsApp com o link
-        const whatsappLink = `https://wa.me/5527992021861?text=${encodeURIComponent(message)}`;
-        window.open(whatsappLink, '_blank');
-    }, function (err) {
-        console.error('Erro ao copiar a mensagem: ', err);
-    });
-}
