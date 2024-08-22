efe01f27447491bf0da20f3cf9d708388987f437
Jr Nolasco <31513935+wmnjunior@users.noreply.github.com>
2024-08-22 18:59:10 -0300

    2.5

--- script.js
+++ script.js
@@ -5,7 +5,6 @@
     if (coupon === validCoupon) {
         const voucherImage = document.getElementById("voucher-image");
         const voucherDate = document.getElementById("voucher-date");
-        const whatsappLink = document.getElementById("whatsapp-link");
 
         // Gerar data e hora atual formatada
         const now = new Date();
@@ -14,11 +13,21 @@
         // Exibir a imagem e a data/hora no canto inferior direito
         voucherDate.textContent = dateString;
         voucherImage.style.display = "block";
-
-        // Atualizar o link do WhatsApp com o novo texto e o link da imagem
-        const whatsappMessage = `Estou entrando em contato para agendar o serviço usando o cupom promocional: *GELSTUDIOTOTI*. Veja o vale: https://encr.pw/VALESTUDIOTOTI`;
-        whatsappLink.href = `https://wa.me/5527992021861?text=${encodeURIComponent(whatsappMessage)}`;
     } else {
         alert("Cupom inválido. Tente novamente.");
     }
 });
+
+function copyToClipboardAndOpenWhatsApp() {
+    // Texto a ser copiado
+    const message = `Estou entrando em contato para agendar o serviço usando o cupom promocional: *GELSTUDIOTOTI*. Veja o vale: https://wmnjunior.github.io/promo-unha-gel/vale.png`;
+
+    // Copiar o texto para a área de transferência
+    navigator.clipboard.writeText(message).then(function () {
+        // Abrir o WhatsApp com o link
+        const whatsappLink = `https://wa.me/5527992021861?text=${encodeURIComponent(message)}`;
+        window.open(whatsappLink, '_blank');
+    }, function (err) {
+        console.error('Erro ao copiar a mensagem: ', err);
+    });
+}
